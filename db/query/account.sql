e907f5377bf8c2a501fa1d6b2150f73f7da4ebdd
kave08 <kave.mailbox@gmail.com>
2024-01-28 15:50:53 +0330

    added get and list function

--- db/query/account.sql
+++ db/query/account.sql
@@ -3,3 +3,9 @@
 INSERT INTO
     account (owner, balance, currency)
 VALUES ($1, $2, $3) RETURNING *;
+
+-- name: GetAccount :one
+SELECT * FROM account WHERE id = $1 LIMIT 1;
+
+-- name: ListAccount :many
+SELECT * FROM account ORDER BY id LIMIT $1 OFFSET $2;
