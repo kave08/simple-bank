e42f0850012dbfc8b10d101632ad6bd1cceef797
kave08 <kave.mailbox@gmail.com>
2024-01-28 16:10:29 +0330

    added update account

--- db/sqlc/account.sql.go
+++ db/sqlc/account.sql.go
@@ -89,3 +89,25 @@ func (q *Queries) ListAccount(ctx context.Context, arg ListAccountParams) ([]Acc
 	}
 	return items, nil
 }
+
+const updateAccount = `-- name: UpdateAccount :one
+UPDATE account SET balance = $2 WHERE id = $1 RETURNING id, owner, balance, currency, created_at
+`
+
+type UpdateAccountParams struct {
+	ID      int64 `json:"id"`
+	Balance int64 `json:"balance"`
+}
+
+func (q *Queries) UpdateAccount(ctx context.Context, arg UpdateAccountParams) (Account, error) {
+	row := q.db.QueryRowContext(ctx, updateAccount, arg.ID, arg.Balance)
+	var i Account
+	err := row.Scan(
+		&i.ID,
+		&i.Owner,
+		&i.Balance,
+		&i.Currency,
+		&i.CreatedAt,
+	)
+	return i, err
+}
