7644ddde9edeea2080194086f5d441ac00771aac
kave08 <kave.mailbox@gmail.com>
2024-01-31 10:42:41 +0330

    added random generator

--- util/random.go
+++ util/random.go
@@ -2,8 +2,11 @@ package util
 
 import (
 	"math/rand"
+	"strings"
 	"time"
 )
+
+const alphabet = "abcdefghijklmnopqrstuwxyz"
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
@@ -12,4 +15,17 @@ func init() {
 // RandomInt generates a random integer between main and max
 func RandomInt(min, max int64) int64 {
 	return min + rand.Int63n(max-min+1)
+}
+
+// RandomString generates a random string of lenght n
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
 }
